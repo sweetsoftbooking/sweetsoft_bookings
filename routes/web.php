96f8e1c3fea41edbd9cef8d31891ec2e71a594e4
unknown <zeusocean1707@gmail.com>
2019-09-18 09:40:37 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::group(['prefix' => 'calendar'], function () {
     Route::get('/','CalendarController@getIndex');
 });
